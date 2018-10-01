47f33ef6474860258cf4a5181057cb6ceb085637
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2018-10-01 17:08:24 +0200

    2. fizzbuzz function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 function fizzbuzz() {
-    for (var i = 1; i <= 100; i++) {
+    for (var i = 1; i <= 90; i++) {
         if (i % 3 == 0) {
             if (i % 5 == 0) {
                 console.log("fizzbuzz");

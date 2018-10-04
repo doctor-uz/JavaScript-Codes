13006c14b4c25c2990903268c3f24d52882a2a83
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2018-10-04 19:41:44 +0200

    Million numbers

---
+++ week1/3-day/javacript/3-number.js
@@ -0,0 +1,13 @@
+function num(n) {
+    if (n <= 0 || isNaN(n)) {
+        console.log("ERROR");
+    } else if (n >= 1000000) {
+        console.log(n);
+    } else {
+        do {
+            n = n * 10;
+        } while (n <= 1000000);
+        console.log(n);
+    }
+}
+num("sss");
